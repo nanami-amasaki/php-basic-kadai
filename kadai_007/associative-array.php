503bbf57b4b91c0eb2b9364c85d48b454d152184
天咲浪海 <145226623+nanami-amasaki@users.noreply.github.com>
2023-11-15 22:12:43 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+            $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+            print_r($data);
+
+
+        ?>
+
+     </p>
+ </body>
+ 
+ </html>
